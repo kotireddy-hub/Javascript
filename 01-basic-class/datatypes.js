bb32fb6f93c6e1ca0cc0a64d2098b8942296d05a
Kotireddy Kamireddy <kotireddy.kamireddy@sap.com>
2023-04-20 08:42:01 +0530

    added datatypes

---
+++ 01-basic-class/datatypes.js
@@ -0,0 +1,56 @@
+//primitive Datatypes
+//number integre
+
+var num = 10;
+console.log(typeof(num),"=======",num);
+
+//number Float
+
+var floatNmu = 1.23456;
+console.log(typeof(floatNmu),"======",floatNmu);
+
+//String
+
+var str = "Kotireddy";
+
+var strSingleQut = 'Kamireddy';
+console.log(typeof(str),"===",str,typeof(strSingleQut),"========",strSingleQut);
+
+//Boolean
+
+var isActive = true;
+console.log(typeof(isActive),"====",isActive);
+
+
+//Null
+
+var a = null;
+console.log(typeof(a),"======",a);
+
+// Undefined
+var b;
+console.log(typeof(b),"====",b);
+
+//Non-primitive Datatypes
+
+//objects
+//Arrays
+
+//Objects ({})
+
+var obj = {
+    "name":"kotireddy",
+    "age":29,
+    "isCar":false
+}
+
+console.log(typeof(obj),"=====",obj)
+
+
+//Arrays ([])
+var arr = [1,"koti",true];
+
+console.log(typeof(arr),"======",arr)
+
+
+
