af2d9cb8ed280c5926f6f87d92751c36e7020378
Kotireddy Kamireddy <kotireddy.kamireddy@sap.com>
2023-04-22 21:47:20 +0530

    added creating external internal inline javascript

---
+++ 02-class/arithmaticOprators.js
@@ -0,0 +1,44 @@
+var a =5;
+var b =10;
+
+//addition 
+var add = a + b;
+//console.log(add);
+
+//subtarction
+var sub = a - b;
+
+//console.log(sub);
+
+//multiplication
+
+var mul = a * b;
+//console.log(mul);
+
+//divinon
+var div = a / b;
+
+//console.log(div);
+
+//modulo
+
+var mud = a % b;
+//console.log(mud);
+
+//increment
+// two type post incre  a ++
+// pre incer ++ a
+
+
+//decrment 
+console.log(--a); // log 5
+
+console.log(a)//4
+
+
+var name = "koti";
+var age = 29;
+
+var totalstr =name+age;
+
+console.log(totalstr);
---
+++ 03-class/index.js
@@ -0,0 +1,29 @@
+var a = 10;
+var b = 20;
+
+console.log(a + b);
+
+//single line comment (//)
+/* multi lines comment 
+-------
+*/
+
+
+//three first letter "chart  / "_"/$ 
+
+var city ="hyd";
+
+console.log("This is new city "+city);
+
+
+// to create multi line in console  (Template Literals)
+
+
+console.log(`
+hdfjkfsdfl
+kjdfskfljf
+kGdkalda;kf
+kdjdh;alkaf
+BLJAS'F;GKS
+KJAGFLKDJLA`)
+
