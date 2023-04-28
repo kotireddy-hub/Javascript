9ca16038c3d0f9c5e90fb7c2835c97b5b6feeeb5
Kotireddy Kamireddy <kotireddy.kamireddy@sap.com>
2023-04-28 08:52:25 +0530

    added srings

---
+++ 04-class/string.js
@@ -0,0 +1,13 @@
+var firstName = "kotireddy";
+var lastName = "kamireddy";
+var fullName = firstName + lastName;
+console.log(fullName);
+
+var full_name_with_space = firstName + " "+lastName; 
+console.log(full_name_with_space);
+
+var name = "kotireddy";
+var num = 123;
+
+var combinedWithNumAndNAme = name + num;
+console.log(combinedWithNumAndNAme);
