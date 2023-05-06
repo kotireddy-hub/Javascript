27ce59e23f1fb37cccb20b74d5036ffe30637500
Kotireddy Kamireddy <kotireddy.kamireddy@sap.com>
2023-05-06 08:43:27 +0530

    functional scope

---
+++ 05-class-variables/functionalScope.js
@@ -0,0 +1,39 @@
+if(true){
+    var a = 20;
+  }
+  console.log(a);
+  
+  var name ="Divya";
+  //var name ="Seshi";
+  function info(){
+  var name = "kotiredy";
+  console.log(name); // kotireddy
+  }
+  console.log(name,"Global Scope"); // Divya
+  info();
+  
+  /* var name
+  info();
+  name = undefined
+  name = "Divya";
+  console.log(name)// Divya
+ 
+
+  Local scope 
+  var name
+  name = un
+  name = kotireddy
+  console.log(name); //kotireddy
+  */
+
+  function infoStr(){
+  let Str = "kotiredy";
+  Str = "Seshi"
+  console.log(Str,"=====");
+  }
+  infoStr();
+  
+  // const 
+
+  const aa = 20;
+  console.log(aa);
