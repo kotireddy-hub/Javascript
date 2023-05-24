ce35b79d18f54a2293d2c12f2ee7c6fa09003910
Kotireddy Kamireddy <kotireddy.kamireddy@sap.com>
2023-05-24 19:44:49 +0530

    static variable

--- 06-expressions/expression.js
+++ 06-expressions/expression.js
@@ -39,4 +39,25 @@ console.log(userInfoDeatils[2][1]);
 
 
 
+var fname ="kotireddy";
+var lname ="kamiredy";
 
+var info = {
+    "firstName":fname,
+    "lastName":lname,
+    "fullName":`${fname} ${lname}`
+}
+console.log(info,"==== info");
+
+var tableNum = 5;
+
+console.log(`${tableNum} X 1 = ${tableNum*1}`);
+console.log(`${tableNum}*2 = 4`)
+console.log(`${tableNum}*3 = 6`)
+console.log(`${tableNum}*4 = 8`)
+console.log(`${tableNum}*5 = 10`)
+console.log(`${tableNum}*6 = 12`)
+console.log(`${tableNum}*7 = 14`)
+console.log(`${tableNum}*8 = 16`)
+console.log(`${tableNum}*9 = 18`)
+console.log(`${tableNum}*10 = 20`)
---
+++ 07-class/variables.js
@@ -0,0 +1,60 @@
+/* creatinng static data */
+var firstName = "Kotireddy";
+var lastName = "Kamireddy";
+
+console.log(`${firstName} ${lastName}`);
+
+
+var fName = "Kotireddy"; //string
+var lName = "Kamireddy"; //string
+let designation = "UI developer";//string
+const company = "xyz";//string
+const Skills = ["HTML","CSS","Javascript"];//object
+let branchCode = "HYD80"; // string
+let emp_id = 20; //number
+
+const userDetails = {
+    firstName:`${fName}`,
+    lastName:lName,
+    "fullName":`${fName} ${lName}`
+};
+
+console.log(userDetails,":: This is User Details");
+
+const tableNumber = 4;
+
+console.log("*****************************");
+
+console.log(`${tableNumber} X 1 = ${tableNumber*1}`);
+console.log(`${tableNumber} X 2 = ${tableNumber*2}`);
+console.log(`${tableNumber} X 3 = ${tableNumber*3}`);
+console.log(`${tableNumber} X 4 = ${tableNumber*4}`);
+console.log(`${tableNumber} X 5 = ${tableNumber*5}`);
+console.log(`${tableNumber} X 6 = ${tableNumber*6}`);
+console.log(`${tableNumber} X 7 = ${tableNumber*7}`);
+console.log(`${tableNumber} X 8 = ${tableNumber*8}`);
+console.log(`${tableNumber} X 9 = ${tableNumber*9}`);
+console.log(`${tableNumber} X 10 = ${tableNumber*10}`);
+
+console.log("*****************************");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
