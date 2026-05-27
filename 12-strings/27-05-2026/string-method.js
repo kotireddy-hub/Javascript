9fc332debf755a3a739cdb7cdea63787953be5b4
Koti Reddy <kotireddyk0485@gmail.com>
2026-05-27 11:28:15 +0530

    string: add string methods

---
+++ 12-strings/27-05-2026/string-method.js
@@ -0,0 +1,37 @@
+// toLowerCase
+console.log('HELLO'.toLowerCase())
+
+//concat 
+
+const str1 = 'Hello'
+const str2 = "Hi";
+
+console.log(str1.concat(str2));
+
+//trim
+const str3 = "             trim            ";
+console.log(str3.trim())
+
+//trimStart
+const str4 = "             trimstart";
+
+console.log(str4.trimStart())
+
+//trimEnd
+const str5 = "timeEnd         ";
+const str6 = "          Welcome         ";
+
+console.log(str5.trimEnd())
+console.log(str6.trimEnd())
+
+
+//padStart
+console.log( 'hello'.padStart(10, '*'));
+
+//padEnd
+console.log( 'hello'.padEnd(10, 'h'));
+
+//charAt
+console.log('hello'.charAt(1));
+
+console.log('hello'.charCodeAt(1))
